b5fe0c44ee9ce6809d1a3c8d037308274ec363a0
Calvin Furano <cfurano@utah.gov>
2023-08-29 16:54:40 -0600

    Basic

--- src/SqlInjector/wwwroot/js/index.js
+++ src/SqlInjector/wwwroot/js/index.js
@@ -1,3 +1,25 @@
-$(() => {
-    console.log("Hello World!")
+import Dexie from "https://unpkg.com/dexie@3.2.4/dist/modern/dexie.mjs";
+
+document.addEventListener("alpine:init", () => {
+  Alpine.data("app", () => ({
+    async init() {
+      // This code will be executed before Alpine
+      // initializes the rest of the component.
+      const db = new Dexie("sql_injector");
+
+      db.version(1).stores({
+        connections: `id++, host, port, database`,
+      });
+
+      // await db.connections.add({
+      //   host: "localhost",
+      //   port: 5432,
+      //   database: "foo_bar",
+      // });
+
+      this.connections = await db.connections.toArray();
+    },
+
+    connections: [],
+  }));
 });
